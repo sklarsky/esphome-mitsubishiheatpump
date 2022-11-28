26d4261691755db191a0e2d36ec839aa87e95ea2
sklarsky <sklarsky@gmail.com>
2022-11-28 11:32:35 -0500

    excessive comments

--- components/mitsubishi_heatpump/climate.py
+++ components/mitsubishi_heatpump/climate.py
@@ -89,6 +89,6 @@ def to_code(config):
     yield climate.register_climate(var, config)
     cg.add_library(
         name="HeatPump",
-        repository="https://github.com/sklarsky/HeatPump"#,
+        repository="https://github.com/sklarsky/HeatPump",
         version="b0b2f0221ad3ab35c695be40b39967fe4bcc4e9b",
     )
